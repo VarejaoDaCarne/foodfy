2c6a2724518a1c8d6eeea86866dcbb3e3646a042
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-08-24 19:02:22 -0300

    reorganizing server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
-const routes = require("./routes")
 const methodOverride = require('method-override')
+const routes = require("./routes")
 const session = require('./config/session')
 
 const server = express()
@@ -11,6 +11,7 @@ server.use((req, res, next) => {
     res.locals.session = req.session
     next()
 })
+
 server.use(express.urlencoded({ extended:true }))
 server.use(express.static('public'))
 server.use(methodOverride('_method'))
