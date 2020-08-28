2c71db185838c9e4f645dbd8db8ff17d9f83bd58
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-08-28 18:28:03 -0300

    Chef put and post  validators

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -2,17 +2,17 @@ const express = require('express')
 const routes = express.Router()
 const multer = require('../app/middlewares/multer')
 
+const { onlyOneOwnRecipeOrAdmin } = require('../app/middlewares/session')
+const RecipeValidator = require('../app/validators/recipe')
 const RecipeController = require('../app/controllers/RecipeController')
 
-const { onlyOneOwnRecipeOrAdmin } = require('../app/middlewares/session')
+routes.get('/recipes', RecipeController.index)
+routes.get('/recipes/create', RecipeController.create) 
+routes.get('/recipes/:id', RecipeController.show) 
+routes.get('/recipes/:id/edit', onlyOneOwnRecipeOrAdmin, RecipeController.edit) 
 
-routes.get("/recipes", RecipeController.index);
-routes.get("/recipes/create", RecipeController.create); 
-routes.get("/recipes/:id", RecipeController.show); 
-routes.get("/recipes/:id/edit", onlyOneOwnRecipeOrAdmin, RecipeController.edit) 
-
-routes.post("/recipes", multer.array("photos", 5), RecipeController.post);
-routes.put("/recipes", multer.array("photos", 5), RecipeController.put);
-routes.delete("/recipes", RecipeController.delete);
+routes.post('/recipes', multer.array('photos', 5), RecipeValidator.post, RecipeController.post)
+routes.put('/recipes', multer.array('photos', 5), RecipeValidator.put, RecipeController.put)
+routes.delete('/recipes', RecipeController.delete)
 
 module.exports = routes
