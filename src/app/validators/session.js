42bf3d4d28b0ddcefe65ecbe310250790c84cb31
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-08-28 18:21:52 -0300

    Session validator

--- src/app/validators/session.js
+++ src/app/validators/session.js
@@ -3,54 +3,54 @@ const { compare } = require('bcryptjs')
 const User = require('../models/User')
 
 async function login(req, res, next) {
-    let { email, password } = req.body
-
     try{
+        const { email, password } = req.body
+
         const user = await User.findOne({ where: {email} })
     
-        if(!user) return res.render("session/login", {
+        if(!user) return res.render('session/login', {
             user: req.body,
-            error: "Usuário não registrado"
+            error: 'Usuário não registrado'
         })
 
         // const passed = await compare(password, user.password)
 
-        // if(!passed) return res.render("session/login", {
+        // if(!passed) return res.render('session/login', {
         //     user: req.body,
-        //     error: "Password mismatch."
-        // })
+        //     error: 'Senha incorreta.'
+        // })''
     
         req.user = user
         
         next()
-    }catch(err) {
-        console.error(err)
-        return res.render("session/login", {
-            user: req.body,
-            error: "Algo deu errado"
+    }catch(error) {
+        console.error(error)
+        return res.render('session/login', {
+            error: 'Algo deu errado'
         })
     }
 }
 
 async function forgot(req, res, next) {
-    const { email } = req.body
- 
     try {
+        const { email } = req.body
+
         let user = await User.findOne({ where: { email }})
     
-        if(!user) return res.render("session/forgot-password", {
-            user: req.body,
-            error: "Email não registrado"
-        })
+        if(!user) 
+            return res.render('session/forgot-password', {
+                user: req.body,
+                error: 'Email não registrado'
+            })
 
         req.user = user
         
         next()
-    }catch(err) {
-        console.error(err)
-        return res.render("session/forgot-password", {
+    }catch(error) {
+        console.error(error)
+        return res.render('session/forgot-password', {
             user: req.body,
-            error: "Algo deu errado"
+            error: 'Algo deu errado'
         })
     }
 }
@@ -61,42 +61,46 @@ async function reset(req, res, next) {
 
         const user = await User.findOne({ where: { email }} )
     
-        if(!user) return res.render("session/password-reset", {
-            user: req.body,
-            token,
-            error: "Usuário não registrado"
-        })
+        if(!user) 
+            return res.render('session/password-reset', {
+                user: req.body,
+                token,
+                error: 'Usuário não registrado'
+            })
     
-        if(password != passwordRepeat) return res.render("session/password-reset", {
-            user: req.body,
-            token,
-            error: 'Senhas não correspondem'
-        })
+        if(password != passwordRepeat) 
+            return res.render('session/password-reset', {
+                user: req.body,
+                token,
+                error: 'Senhas não correspondem'
+            })
     
-        if(token != user.reset_token) return res.render("session/password-reset", {
-            user: req.body,
-            token,
-            error: 'Token inválido! Tente novamente'
-        })
+        if(token != user.reset_token) 
+            return res.render('session/password-reset', {
+                user: req.body,
+                token,
+                error: 'Token inválido! Tente novamente'
+            })
     
         let now = new Date()
         now = now.setHours(now.getHours())
     
-        if(now > user.reset_token_expires) return res.render("session/password-reset", {
-            user: req.body,
-            token,
-            error: 'Token expirou! Solicite o formulário de resetar a senha novamente'
-        })
+        if(now > user.reset_token_expires) 
+            return res.render('session/password-reset', {
+                user: req.body,
+                token,
+                error: 'Token expirou! Solicite o formulário de resetar a senha novamente'
+            })
     
         req.user = user
     
         next()
     } catch (error) {
         console.error(error)
-        return res.render("session/password-reset", {
+        return res.render('session/password-reset', {
             user: req.body,
             token,
-            error: "Algo deu errado"
+            error: 'Algo deu errado'
         })
     }
 }
