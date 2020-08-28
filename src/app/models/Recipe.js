fbfdfa78e65da23b48fdf8f445b4b18553767636
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-08-28 18:19:29 -0300

    Recipe model adapted to new base model

--- src/app/models/Recipe.js
+++ src/app/models/Recipe.js
@@ -1,80 +1,38 @@
 const db = require('../../config/db')
 
+const Base = require('./Base')
+Base.init({ table: 'recipes' })
+
 module.exports = {
-    all() {
+    ...Base,
+    async findAll() {
         try {
-            return db.query(`
-            SELECT recipes.*, chefs.name AS chef_name 
-            FROM recipes
-            LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
-            ORDER BY created_at DESC
-            `)   
+            const query = `
+                SELECT ${this.table}.*, chefs.name AS chef_name 
+                FROM ${this.table}
+                LEFT JOIN chefs ON (${this.table}.chef_id = chefs.id)
+                ORDER BY created_at DESC
+            `
+
+            const results = await db.query(query)
+
+            return results.rows
         } catch (error) {
             console.error(error)
         }
     },
-    create(data) {
-        try{
-            const query = `
-                INSERT INTO recipes (
-                    chef_id,
-                    title,
-                    user_id,
-                    ingredients,
-                    preparation,
-                    information
-                ) VALUES ($1, $2, $3, $4, $5, $6)
-                RETURNING id
-            `
-            const values = [
-                data.chef,
-                data.title,
-                data.user_id,
-                data.ingredients,
-                data.preparation,
-                data.information
-            ]
-
-            return db.query(query, values)   
-        }catch(err) {
-            console.error(err)
-        }
-    },
-    find(id) {
+    async findOne(id) {
         try {
-            return db.query(`
-            SELECT recipes.*, chefs.name AS chef_name 
-            FROM recipes
-            LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
-            WHERE recipes.id = $1`, [id])
+            const results = await db.query(`
+                SELECT ${this.table}.*, chefs.name AS chef_name 
+                FROM ${this.table}
+                LEFT JOIN chefs ON (${this.table}.chef_id = chefs.id)
+                WHERE ${this.table}.id = $1
+            `, [id])
+
+            return results.rows[0]
         } catch (error) {
             console.error(error)
-        }
-    },
-    update(data) {
-        try{
-            const query = `
-                UPDATE recipes SET
-                    chef_id=($1),
-                    title=($2),
-                    ingredients=($3),
-                    preparation=($4),
-                    information=($5)
-                WHERE id = $6
-            `
-
-            const values = [
-                data.chef,
-                data.title,
-                data.ingredients,
-                data.preparation,
-                data.information,
-                data.id
-            ]
-            
-            return db.query(query, values)
-        }catch(err) {
-            console.error(err)
         }
     },
     async delete(id) {
@@ -83,23 +41,9 @@ module.exports = {
                 DELETE FROM recipe_files WHERE recipe_id = $1
             `,[id])
 
-            return db.query(`DELETE FROM recipes WHERE id = $1`, [id])
+            return db.query(`DELETE FROM ${this.table} WHERE id = $1`, [id])
         }catch(err) {
             console.error(err)
-        }
-    },
-   chefsSelectOptions() {
-        return db.query(`SELECT name, id FROM chefs`)
-    },
-    files(id) {
-        try {
-            return db.query(`
-            SELECT files.*, recipe_id, file_id
-            FROM files
-            LEFT JOIN recipe_files ON (files.id = recipe_files.file_id)
-            WHERE recipe_id = $1`, [id])
-        } catch (error) {
-            console.error(error)
         }
     }
 }
