0918702a4680417901a42402fb74e9be7767372e
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-08-28 18:19:20 -0300

    Chef model adapted to new base model

--- src/app/models/Chef.js
+++ src/app/models/Chef.js
@@ -1,74 +1,38 @@
 const db = require('../../config/db')
 
+const Base = require('./Base')
+
+Base.init({ table: 'chefs' })
+
 module.exports = {
-    all() {
+    ...Base,
+    async findAll() {
         try {
-            return db.query(`
-            SELECT chefs.*, count(recipes) AS total_recipes 
-            FROM chefs 
-            LEFT JOIN recipes on (chefs.id = recipes.chef_id)
-            GROUP BY chefs.id
-            ORDER BY total_recipes DESC`)
+            const results = await db.query(`
+                SELECT ${this.table}.*, count(recipes) AS total_recipes
+                FROM ${this.table}
+                LEFT JOIN recipes ON (${this.table}.id = recipes.chef_id)
+                GROUP BY ${this.table}.id
+                ORDER BY total_recipes DESC
+            `)
+
+            return results.rows
         } catch (error) {
             console.error(error)
         }
     },
-    create({name, file_id}) {
-        try{
-            const query = `
-                INSERT INTO chefs (
-                    name,
-                    file_id
-                ) VALUES ($1, $2)
-                RETURNING id
-            `
-            const values = [
-                name,
-                file_id
-            ]
+    async chefRecipes(id) {
+        try {
+            const results = await db.query(`
+                SELECT recipes.*
+                FROM recipes
+                WHERE chef_id = $1
+                ORDER BY created_at DESC
+            `, [id])
 
-            return db.query(query, values)
-        }catch(err) {
-            console.error(err)
+            return results.rows
+        } catch (error) {
+            console.error(error)
         }
-    },
-    find(id) {
-        return db.query(`
-        SELECT * 
-        FROM chefs
-        WHERE id = $1`, [id])
-    },
-
-    update(data) {
-        try{
-            const query = `
-                UPDATE chefs SET
-                    name=($1),
-                    file_id=($2)
-                WHERE id = $3
-            `
-
-            const values = [
-                data.name,
-                data.file_id,
-                data.id
-            ]
-
-            return db.query(query, values)
-        }catch(err) {
-            console.error(err)
-        }
-    },
-     async delete(id) {
-        return db.query(`DELETE FROM chefs WHERE id = $1`, [id])
-    },
-    recipesChef(id) {
-        return db.query(`
-        SELECT recipes.* FROM recipes WHERE chef_id = $1 ORDER BY created_at DESC`, [id])
-    },
-    files(id) {
-        return db.query(`
-            SELECT * FROM files WHERE id = $1
-        `, [id])
     }
 }
