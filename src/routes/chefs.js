888595430bb4590714b745c22f6b162d702eff6f
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-08-28 18:27:37 -0300

    Chef put, post and delete validators

--- src/routes/chefs.js
+++ src/routes/chefs.js
@@ -2,17 +2,17 @@ const express = require('express')
 const routes = express.Router()
 const multer = require('../app/middlewares/multer')
 
+const { onlyUserOrAdmin } = require('../app/middlewares/session')
+const ChefValidator = require('../app/validators/chef')
 const ChefController = require('../app/controllers/ChefController')
 
-const { onlyUserOrAdmin } = require('../app/middlewares/session')
+routes.get('/chefs', ChefController.index)
+routes.get('/chefs/create', onlyUserOrAdmin, ChefController.create) 
+routes.get('/chefs/:id', ChefController.show) 
+routes.get('/chefs/:id/edit', onlyUserOrAdmin, ChefController.edit) 
 
-routes.get("/chefs", ChefController.index);
-routes.get("/chefs/create", onlyUserOrAdmin, ChefController.create); 
-routes.get("/chefs/:id", ChefController.show); 
-routes.get("/chefs/:id/edit", onlyUserOrAdmin, ChefController.edit) 
-
-routes.post("/chefs", multer.array("photos", 1), onlyUserOrAdmin, ChefController.post);
-routes.put("/chefs", multer.array("photos", 1), onlyUserOrAdmin, ChefController.put);
-routes.delete("/chefs", onlyUserOrAdmin, ChefController.delete);
+routes.post('/chefs', multer.array('photos', 1), onlyUserOrAdmin, ChefValidator.post, ChefController.post)
+routes.put('/chefs', multer.array('photos', 1), onlyUserOrAdmin, ChefValidator.put, ChefController.put)
+routes.delete('/chefs', onlyUserOrAdmin, ChefValidator.delete, ChefController.delete)
 
 module.exports = routes
