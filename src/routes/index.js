95d777f9fb8d22b7ab61c900b4ae35b85e8c97e6
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-08-31 18:46:14 -0300

    Index routes refactored

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,16 @@
 const express = require('express')
 const routes = express.Router()
 
-const HomeController = require('../app/controllers/HomeController')
-
 const chefs = require('./chefs')
+const home = require('./home')
 const recipes = require('./recipes')
 const users = require('./users')
 
 const { onlyUsers } = require('../app/middlewares/session')
 
+routes.use('/', home)
 routes.use('/admin', onlyUsers, chefs)
 routes.use('/admin', onlyUsers, recipes)
 routes.use('/', users)
-
-routes.get('/', HomeController.index) 
-routes.get('/about', HomeController.about) 
-routes.get('/recipes', HomeController.recipes) 
-routes.get('/recipes/:id', HomeController.recipe) 
-routes.get('/chefs', HomeController.chefs)
-routes.get('/chefs/:id', HomeController.chef)
-routes.get('/search', HomeController.search)
 
 module.exports = routes
