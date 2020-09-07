3bac59d17e16d7e5d641c845398c007abee95817
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-09-07 14:36:51 -0300

    chef model new find

--- src/app/models/Chef.js
+++ src/app/models/Chef.js
@@ -9,10 +9,10 @@ module.exports = {
     async findAll() {
         try {
             const results = await db.query(`
-                SELECT ${this.table}.*, count(recipes) AS total_recipes
-                FROM ${this.table}
-                LEFT JOIN recipes ON (${this.table}.id = recipes.chef_id)
-                GROUP BY ${this.table}.id
+                SELECT chefs.*, count(recipes) AS total_recipes
+                FROM chefs
+                LEFT JOIN recipes ON (chefs.id = recipes.chef_id)
+                GROUP BY chefs.id
                 ORDER BY total_recipes DESC
             `)
 
@@ -21,11 +21,28 @@ module.exports = {
             console.error(error)
         }
     },
+    async find(id) {
+        try {
+          const results = await db.query(`
+            SELECT chefs.*, count(recipes) AS total_recipes
+            FROM chefs
+            LEFT JOIN recipes ON (chefs.id = recipes.chef_id)
+            WHERE chefs.id = $1
+            GROUP BY chefs.id
+            ORDER BY total_recipes DESC
+        `, [id])
+
+            return results.rows[0]
+        } catch (error) {
+            console.error(error)
+        }
+      },
     async chefRecipes(id) {
         try {
             const results = await db.query(`
-                SELECT recipes.*
+                SELECT recipes.*, chefs.name AS chef_name 
                 FROM recipes
+                LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
                 WHERE chef_id = $1
                 ORDER BY created_at DESC
             `, [id])
