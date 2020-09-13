0cd4f61ab37395d9fc535e56e2ae8964e577204d
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-09-13 11:52:36 -0300

    recipes routes middlewares and validators refactored

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -2,17 +2,17 @@ const express = require('express')
 const routes = express.Router()
 const multer = require('../app/middlewares/multer')
 
-const { onlyOneOwnRecipeOrAdmin } = require('../app/middlewares/session')
+const { ownerOfRecipeOrAdmin } = require('../app/middlewares/session')
 const RecipeValidator = require('../app/validators/recipe')
 const RecipeController = require('../app/controllers/RecipeController')
 
 routes.get('/recipes', RecipeController.index)
 routes.get('/recipes/create', RecipeController.create) 
-routes.get('/recipes/:id', RecipeController.show) 
-routes.get('/recipes/:id/edit', onlyOneOwnRecipeOrAdmin, RecipeController.edit) 
+routes.get('/recipes/:id', RecipeValidator.show, RecipeController.show) 
+routes.get('/recipes/:id/edit', ownerOfRecipeOrAdmin, RecipeController.edit) 
 
 routes.post('/recipes', multer.array('photos', 5), RecipeValidator.post, RecipeController.post)
-routes.put('/recipes', multer.array('photos', 5), RecipeValidator.put, RecipeController.put)
-routes.delete('/recipes', RecipeController.delete)
+routes.put('/recipes', ownerOfRecipeOrAdmin, multer.array('photos', 5), RecipeValidator.put, RecipeController.put)
+routes.delete('/recipes', ownerOfRecipeOrAdmin, RecipeController.delete)
 
 module.exports = routes
