de780ac9f9b2dac04aea231f6f3fb16dc8ca29ab
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-09-13 12:57:08 -0300

    database

--- database.sql
+++ database.sql
@@ -64,8 +64,7 @@ CREATE TABLE "session" (
   "expire" timestamp(6) NOT NULL
 )
 WITH (OIDS=FALSE);
-ALTER TABLE "session" 
-ADD CONSTRAINT "session_pkey"
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey"
 PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
 
 ALTER TABLE "chefs" 
