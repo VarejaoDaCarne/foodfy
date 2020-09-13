832eeac4144aee829ec0075b622b51f10d1542e2
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-09-13 11:52:42 -0300

    users routes middlewares and validators refactored

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,11 +6,12 @@ const SessionController = require('../app/controllers/SessionController')
 const UserController = require('../app/controllers/UserController')
 
 const SessionValidator = require('../app/validators/session')
+const ProfileValidator = require('../app/validators/profile')
 const UserValidator = require('../app/validators/users')
 
-const { isLoggedRedirectToUsers, onlyUserOrAdmin  } = require('../app/middlewares/session')
+const { isLoggedRedirectToProfile, onlyAdmin } = require('../app/middlewares/session')
 
-routes.get('/login', isLoggedRedirectToUsers, SessionController.loginForm)
+routes.get('/login', isLoggedRedirectToProfile, SessionController.loginForm)
 routes.post('/login', SessionValidator.login, SessionController.login)
 routes.post('/logout', SessionController.logout)
 
@@ -19,15 +20,15 @@ routes.get('/password-reset', SessionController.resetForm)
 routes.post('/password-forgot', SessionValidator.forgot, SessionController.forgot)
 routes.post('/password-reset', SessionValidator.reset, SessionController.reset)
 
-routes.get('/admin/profile/:id', onlyUserOrAdmin, UserValidator.show, ProfileController.index) 
-routes.put('/admin/profile', UserValidator.update, ProfileController.put)
+routes.get('/admin/profile/:id', ProfileValidator.show, UserValidator.show, ProfileController.index) 
+routes.put('/admin/profile', ProfileValidator.put, ProfileController.put)
 
-routes.get('/admin/users/register', onlyUserOrAdmin, UserController.registerForm)
+routes.get('/admin/users/register', onlyAdmin, UserController.registerForm)
 routes.get('/admin/users', UserController.list)
-routes.get('/admin/users/:id', onlyUserOrAdmin, UserValidator.show, UserController.show)
+routes.get('/admin/users/:id', onlyAdmin, UserValidator.show, UserController.show)
 
-routes.post('/admin/users', onlyUserOrAdmin, UserValidator.post, UserController.post)
-routes.put('/admin/users', onlyUserOrAdmin, UserValidator.update, UserController.put) 
-routes.delete('/admin/users', onlyUserOrAdmin, UserValidator.remove,UserController.delete)
+routes.post('/admin/users', onlyAdmin, UserValidator.post, UserController.post)
+routes.put('/admin/users', onlyAdmin, UserValidator.update, UserController.put) 
+routes.delete('/admin/users', onlyAdmin, UserValidator.remove,UserController.delete)
 
 module.exports = routes
